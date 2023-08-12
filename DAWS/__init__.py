ef09ee278aff648c5d651d37d1855b9671152677
Satwik <satwikarnav@gmail.com>
2023-08-12 19:59:45 +0530

    changes

--- DAWS/__init__.py
+++ DAWS/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
 
-@app.route("/")
+class Upload_Image(FlaskForm):
+    file = FileField("asdf")
+    submit = SubmitField("Upload Files")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    form = Upload_Image()
 
-# bce1103
+    return render_template("index.html", form=form)
